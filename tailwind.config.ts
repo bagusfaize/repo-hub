e6011212e8f7359a323dd2c07a39828ff622914b
bagus faize <bagusfaize@gmail.com>
2025-04-30 14:41:07 +0700

    chore: init app and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        "sunflower": "#F9A826",
+        "sunflower-darker" : "#d19432"
+      }
     },
   },
   plugins: [],
